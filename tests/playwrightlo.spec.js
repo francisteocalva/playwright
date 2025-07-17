6abae36473438e2cdbb5f147154fda456e57ff57
francis <francis.teo.calva@gmail.com>
2025-07-17 12:17:59 +0800

    test

--- tests/playwrightlo.spec.js
+++ tests/playwrightlo.spec.js
@@ -9,7 +9,16 @@ test.only('specloc test', async function ({browser}) {
     await page.getByLabel("Check me out if you Love IceCreams!").check();
     await page.getByLabel("Gender").selectOption("Female");
     await page.getByLabel("Student").check();
-
+    //placeholder
+    await page.getByPlaceholder("Password").fill("test");
+    //role submit button
+    await page.getByRole("button", {name:'Submit'}).click();
+    //message getbytext
+    await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+    //role link
+    await page.getByRole("link",{name: 'Shop'}).click();
+    //chain name
+    await page.locator("app-card").filter({hasText:'Samsung Note 8'}).getByRole("button").click();
 
 
     //test
