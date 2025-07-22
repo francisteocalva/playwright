dae2f6595afa487e2a165a597992fc9dd9f1c4f5
francis <francis.teo.calva@gmail.com>
2025-07-22 12:19:56 +0800

    m

--- tests/calendar.spec.js
+++ tests/calendar.spec.js
@@ -1,14 +1,14 @@
 const{test,expect} = require ("@playwright/test");
 
 
-test.only('calendar', async function ({browser }) {
+test('calendar', async function ({browser }) {
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
     const year = "2028"
     const month = '5'
     const day = '15'
-
+    const datesva = [month, day, year]
     await page.locator(".react-date-picker__inputGroup").click();
     await page.locator(".react-calendar__navigation__label").click();
     await page.locator(".react-calendar__navigation__label").click();
@@ -18,10 +18,45 @@ test.only('calendar', async function ({browser }) {
 
     await page.locator("//abbr[text()='"+day+"']").click();
 
-    const retVal = await page.locator(".react-date-picker__inputGroup").textContent();
-    console.log(retVal)
-    expect(retVal).toEqual("07/21/2025");
+    const retVal =  page.locator(".react-date-picker__inputGroup input");
 
-    await page.pause();
+    for(let index = 1; index< await retVal.count(); index++){
+        const value = await retVal.nth(index).getAttribute("value");
+        expect(value).toEqual(datesva[index-1])
+        console.log(value)
+    }
 
+    //console.log(retVal)
+   //expect(retVal).toEqual("07/21/2025");
+
+   await page.pause();
+
+
+   
 })
+
+
+test.only("Calendar validations",async({page})=>
+{
+ 
+    const monthNumber = "6";
+    const date = "15";
+    const year = "2027";
+    const expectedList = [monthNumber,date,year];
+    await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+    await page.locator(".react-date-picker__inputGroup").click();
+    await page.locator(".react-calendar__navigation__label").click();
+    await page.locator(".react-calendar__navigation__label").click();
+    await page.getByText(year).click();
+    await page.locator(".react-calendar__year-view__months__month").nth(Number(monthNumber)-1).click();
+    await page.locator("//abbr[text()='"+date+"']").click();
+    const inputs = await page.locator(".react-date-picker__inputGroup input");
+    for (let index = 0; index <inputs.length; index++)
+    {
+        const value =inputs[index].getAttribute("value");
+        expect(value).toEqual(expectedList[index]);
+    }
+ 
+ 
+})
+
