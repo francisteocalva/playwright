d845cfad683623264d2fc1b4887a742c2120f8f1
francis <francis.teo.calva@gmail.com>
2025-08-28 11:51:53 +0800

    asdf

--- pageobject/DashboardF.js
+++ pageobject/DashboardF.js
@@ -8,7 +8,6 @@ class dashboardF {
 
     async clickButton(choose) {
         const parentC = await this.parentBody.count();
-
         for (let i = 0; i < parentC; i++) {
             const reTval = await this.parentBody.locator("b").nth(i).textContent();
             if (reTval === choose) {
